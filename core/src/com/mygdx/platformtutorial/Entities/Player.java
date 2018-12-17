2f478d59c066ed6a36f3c9b59cae78b7853ed37e
jordanraitt <jordanraitt@hotmail.com>
2018-12-17 16:54:57 +0000

    idle animation implemented, crashing after loop

--- core/src/com/mygdx/platformtutorial/Entities/Player.java
+++ core/src/com/mygdx/platformtutorial/Entities/Player.java
@@ -3,20 +3,39 @@ package com.mygdx.platformtutorial.Entities;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.mygdx.platformtutorial.World.GameMap;
 
 public class Player extends Entity {
 
+    private static final int FRAME_COLS = 18, FRAME_ROWS = 35;
     private static final int SPEED = 100;
     private static final int JUMP_VELOCITY = 6;
+    private Animation<TextureRegion> animation;
+    private Texture image;
+    private float elapsedTime;
 
-    Texture image;
 
 
     public Player(float x, float y, GameMap map) {
         super(x, y, EntityType.PLAYER, map);
-        image = new Texture("player.png");
+        image = new Texture("idleam.png");
+
+        TextureRegion[][] tmpFrames = TextureRegion.split(image, FRAME_COLS, FRAME_ROWS);
+
+        TextureRegion[] image = new TextureRegion[FRAME_COLS * FRAME_ROWS];
+        int index = 0;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 2; j++) {
+               image[index++] = tmpFrames[j][i];
+            }
+        }
+
+        animation = new Animation<TextureRegion>(1f/6f, image);
+
     }
 
     @Override
@@ -46,6 +65,10 @@ public class Player extends Entity {
 
     @Override
     public void render(SpriteBatch batch) {
-        batch.draw(image, position.x, position.y, getWidth(), getHeight());
+        elapsedTime += Gdx.graphics.getDeltaTime();
+        TextureRegion currentFrame = animation.getKeyFrame(elapsedTime,true);
+//        batch.draw(animation.getKeyFrame(elapsedTime, true), position.x, position.y);
+        batch.draw(currentFrame, position.x, position.y);
     }
 }
+
