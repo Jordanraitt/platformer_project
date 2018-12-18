1f32bbe5063f4da19355f5c80145706854197566
Nyalls Hemingway <hemingwaynyalls@gmail.com>
2018-12-18 16:01:16 +0000

    implemented welcome screen

--- core/src/com/mygdx/platformtutorial/Screens/MainMenuScreen.java
+++ core/src/com/mygdx/platformtutorial/Screens/MainMenuScreen.java
@@ -1,8 +1,28 @@
 package com.mygdx.platformtutorial.Screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.mygdx.platformtutorial.PlatformerGame;
 
 public class MainMenuScreen implements Screen {
+
+
+    protected final PlatformerGame game;
+    OrthographicCamera camera;
+
+    public MainMenuScreen(PlatformerGame game) {
+        this.game = game;
+
+        float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getHeight();
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 720, 480);
+        camera.update();
+    }
 
     public void show() {
 
@@ -11,8 +31,23 @@ public class MainMenuScreen implements Screen {
     @Override
     public void render(float delta) {
 
-    }
+        Gdx.gl.glClearColor(0, 0, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        camera.update();
+        game.getBatch().setProjectionMatrix(camera.combined);
+
+        game.getBatch().begin();
+        game.getFont().draw(game.getBatch(), "Welcome! ", 100, 150);
+        game.getFont().draw(game.getBatch(), "Hit ENTER to start the game!!", 100, 100);
+        game.getBatch().end();
+
+        if (Gdx.input.isKeyPressed(Input.Keys.ENTER)) {
+            game.setScreen(new GameScreen(game));
+            dispose();
+        }
+
+    }
     @Override
     public void resize(int width, int height) {
 
