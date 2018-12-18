628f53874f301fe48c750c27d759b6c13f7ed5c8
jordanraitt <jordanraitt@hotmail.com>
2018-12-18 14:16:33 +0000

    flip works, animation broken

--- core/src/com/mygdx/platformtutorial/Entities/Player.java
+++ core/src/com/mygdx/platformtutorial/Entities/Player.java
@@ -6,41 +6,86 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.Array;
 import com.mygdx.platformtutorial.World.GameMap;
 
 public class Player extends Entity {
 
-//    private static final int FRAME_COLS = 18, FRAME_ROWS = 35;
+//    private Animation idleAnimation;
+//    private Animation jumpingAnimation;
+//    private Animation tunningAnimation;
 
+    public enum State { FALLING, JUMPING, STANDING, RUNNING};
+    private Animation<TextureRegion> idleAnimation;
+    private boolean runningRight;
+    private Texture idleImage;
     private static final int SPEED = 100;
     private static final int JUMP_VELOCITY = 6;
     private boolean isDead = false;
-
-    private Animation<TextureRegion> idleAnimation;
-    private Texture idleImage;
     private float elapsedTime;
+    public State currentState;
+    public State previousState;
+
+
+
 
 
 
     public Player(float x, float y, GameMap map) {
         super(x, y, EntityType.PLAYER, map);
         idleImage = new Texture("idleam2.png");
+        runningRight = true;
+        elapsedTime = 0;
+        currentState = State.STANDING;
+        previousState = State.STANDING;
 
+        ///////////////////////FRAME ANIMATION
+//        Array<TextureRegion> frames = new Array<TextureRegion>();
+//        TextureRegion[][] tmpFrames = TextureRegion.split(idleImage, 18, 35);
+//        TextureRegion[] frames = new TextureRegion[6];
+//        int index = 0;
+//        for (int i = 0; i < 3; i++) {
+//            for (int j = 0; j < 2; j++) {
+//                frames[index++] = tmpFrames[j][i];
+//            }
+//        }
+//        frames.add( new TextureRegion(idleImage));
         TextureRegion[][] tmpFrames = TextureRegion.split(idleImage, 18, 35);
-
         TextureRegion[] idleFrames = new TextureRegion[6];
         int index = 0;
-
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 2; j++) {
-               idleFrames[index++] = tmpFrames[j][i];
+                idleFrames[index++] = tmpFrames[j][i];
             }
         }
 
-        idleAnimation = new Animation<TextureRegion>(1f/6f, idleFrames);
-//        elapsedTime = 0f;
+        idleAnimation = new Animation(1f/6f, idleFrames);
+///////////////////////
+    }
 
+    public TextureRegion getFrame(float deltaTime) {
+        TextureRegion region;
+        switch(currentState){
+            case RUNNING:
+               region = idleAnimation.getKeyFrame(elapsedTime, true);
+               break;
+               case STANDING:
+            default:
+                region = idleAnimation.getKeyFrame(elapsedTime, true);
+        }
 
+        if ((Gdx.input.isKeyPressed(Keys.LEFT) || !runningRight) && !region.isFlipX()){
+            region.flip(true, false);
+            runningRight = false;
+        }
+        else if ((Gdx.input.isKeyPressed(Keys.RIGHT) || runningRight)&& region.isFlipX()) {
+            region.flip( true, false);
+            runningRight = true;
+        }
+
+        elapsedTime = currentState == previousState ? elapsedTime + deltaTime : 0;
+        previousState = currentState;
+        return region;
     }
 
     @Override
@@ -58,7 +103,7 @@ public class Player extends Entity {
     public void render(SpriteBatch batch) {
         elapsedTime += Gdx.graphics.getDeltaTime();
         TextureRegion currentFrame = idleAnimation.getKeyFrame(elapsedTime,true);
-        batch.draw(currentFrame, position.x, position.y);
+        batch.draw(this.getFrame(elapsedTime), position.x, position.y);
 
     }
 
@@ -71,6 +116,7 @@ public class Player extends Entity {
     }
 
     public void moveLeft(float deltaTime) {
+
         if ((Gdx.input.isKeyPressed(Keys.LEFT) && grounded) || (Gdx.input.isKeyPressed(Keys.A) && grounded))
             moveX((-SPEED - 20) * deltaTime);
 
